d7225e5e453fc4f48199a97d15d339f8b4a28831
rddeters <134734702+rddeters@users.noreply.github.com>
2023-11-12 12:41:16 -0600

    Add files via upload

---
+++ Migrations/20231112171140_Initial.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VolleyballFinal.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlayerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Number = table.Column<int>(type: "int", nullable: false),
+                    TeamName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Height = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Statistics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlayerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TotalPoints = table.Column<int>(type: "int", nullable: false),
+                    AttackPoints = table.Column<int>(type: "int", nullable: false),
+                    BlockPoints = table.Column<int>(type: "int", nullable: false),
+                    ServePoints = table.Column<int>(type: "int", nullable: false),
+                    Efficiency = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statistics", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeamName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LeagueType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Player");
+
+            migrationBuilder.DropTable(
+                name: "Statistics");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+        }
+    }
+}
---
+++ Migrations/20231112181944_SeedTeamsData.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VolleyballFinal.Migrations
+{
+    public partial class SeedTeamsData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Teams",
+                columns: new[] { "Id", "Category", "Gender", "LeagueType", "Location", "TeamName" },
+                values: new object[,]
+                {
+                    { 1, "Indoor", "Men", "2020 Olympics", "Argentina", "Argentina" },
+                    { 2, "Indoor", "Men", "2020 Olympics", "Brazil", "Brazil" },
+                    { 3, "Indoor", "Men", "2020 Olympics", "Canada", "Canada" },
+                    { 4, "Indoor", "Men", "2020 Olympics", "France", "France" },
+                    { 5, "Indoor", "Men", "2020 Olympics", "Iran", "Iran" },
+                    { 6, "Indoor", "Men", "2020 Olympics", "Italy", "Italy" },
+                    { 7, "Indoor", "Men", "2020 Olympics", "Japan", "Japan" },
+                    { 8, "Indoor", "Men", "2020 Olympics", "Poland", "Poland" },
+                    { 9, "Indoor", "Men", "2020 Olympics", "Russia", "ROC" },
+                    { 10, "Indoor", "Men", "2020 Olympics", "Tunisia", "Tunisia" },
+                    { 11, "Indoor", "Men", "2020 Olympics", "United States", "USA" },
+                    { 12, "Indoor", "Men", "2020 Olympics", "Venezuela", "Venezuela" },
+                    { 13, "Indoor", "Women", "Big 12 Conference", "University of Texas, Austin TX", "Texas" },
+                    { 14, "Indoor", "Women", "Big 12 Conference", "University of Kansas, Lawrence KS", "Kansas" },
+                    { 15, "Indoor", "Women", "Big 12 Conference", "Brigham Young University, Provo UT", "BYU" },
+                    { 16, "Indoor", "Women", "Big 12 Conference", "University of Central Florida, Orlando FL", "UCF" },
+                    { 17, "Indoor", "Women", "Big 12 Conference", "Baylor University, Waco TX", "Baylor" },
+                    { 18, "Indoor", "Women", "Big 12 Conference", "Iowa State University, Ames IA", "Iowa State" },
+                    { 19, "Indoor", "Women", "Big 12 Conference", "University of Houston, Houston TX", "Houston" },
+                    { 20, "Indoor", "Women", "Big 12 Conference", "Kansas State University, Manhattan KS", "Kansas State" },
+                    { 21, "Indoor", "Women", "Big 12 Conference", "Texas Christian University, Fort Worth TX", "TCU" },
+                    { 22, "Indoor", "Women", "Big 12 Conference", "University of Cincinnati, Cincinnati OH", "Cincinnati" },
+                    { 23, "Indoor", "Women", "Big 12 Conference", "University of Oklahoma, Norman OK", "Oklahoma" },
+                    { 24, "Indoor", "Women", "Big 12 Conference", "West Virginia University, Morgantown WV", "West Virginia" },
+                    { 25, "Indoor", "Women", "Big 12 Conference", "Texas Tech University, Lubbock TX", "Texas Tech" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 25);
+        }
+    }
+}
