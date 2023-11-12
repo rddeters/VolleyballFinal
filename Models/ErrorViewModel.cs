ec61ceb4d979238bf1ace1e677379bd0b8d7408e
rddeters <134734702+rddeters@users.noreply.github.com>
2023-11-12 12:41:56 -0600

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace VolleyballFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
