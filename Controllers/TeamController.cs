a17a0034cfbd6f507812b3c07da4de7cb700f4b2
rddeters <134734702+rddeters@users.noreply.github.com>
2023-11-19 21:38:00 -0600

    Update TeamController.cs

--- Controllers/TeamController.cs
+++ Controllers/TeamController.cs
@@ -3,7 +3,7 @@ using VolleyballFinal.Models;
 
 namespace VolleyballFinal.Controllers
 {
-    public class TeamController: Controller
+    public class TeamController : Controller
     {
         private TeamContext context { get; set; }
         public TeamController(TeamContext ctx) => context = ctx;
@@ -14,11 +14,41 @@ namespace VolleyballFinal.Controllers
             return View(teams); 
         }
 
-        [HttpGet]
-        public IActionResult Add()
+        public IActionResult FilterByLeague(string leaguetype)
         {
-            ViewBag.Action = "Add";
-            return View("Edit", new Team());
+            IEnumerable<Team> teams;
+
+            if (leaguetype.ToLower() == "all")
+            {
+                teams = context.Teams.OrderBy(t => t.TeamName).ToList();
+            }
+            else
+            {
+                leaguetype = leaguetype.ToLower();
+                teams = context.Teams.Where(t => t.LeagueType.ToLower() == leaguetype)
+                                     .OrderBy(t => t.TeamName).ToList();
+            }
+
+            return View("Index", teams);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var team = context.Teams.FirstOrDefault(t => t.Id == id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            var players = context.Player.Where(p => p.TeamName == team.TeamName).ToList();
+
+            var viewModel = new TeamDetailsViewModel
+            {
+                Team = team,
+                Players = players
+            };
+
+            return View(viewModel);
         }
 
         [HttpGet]
