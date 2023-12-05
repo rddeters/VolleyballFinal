5856f27db1ce21f1bc2e02f8e6f8adf18665bd34
emmac <steph.sis0@gmail.com>
2023-12-04 18:10:41 -0600

    Update to AdminController and AdminViewModel

---
+++ Models/Admin/AdminViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace VolleyballFinal.Models.Admin
+{
+    public class AdminViewModel
+    {
+        public IEnumerable<User> Users { get; set; } = null!;
+        public IEnumerable<IdentityRole> Roles { get; set; } = null!;
+    }
+}
