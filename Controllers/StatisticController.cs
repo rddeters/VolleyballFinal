b3269a9d36ebe8c737b9eb8bb5a8db92170209ea
rddeters <134734702+rddeters@users.noreply.github.com>
2023-12-07 23:06:42 -0600

    Update StatisticController.cs

--- Controllers/StatisticController.cs
+++ Controllers/StatisticController.cs
@@ -1,9 +1,93 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VolleyballFinal.Models;
 
 namespace VolleyballFinal.Controllers
 {
     public class StatisticController : Controller
     {
-        //not sure what we will need here for the statistics. Discuss with River
+        private TeamContext context { get; set; }
+        public StatisticController(TeamContext ctx) => context = ctx;
+
+        public IActionResult Index()
+        {
+            var players = context.Player.ToList();
+            return View(players);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var statistic = context.Statistics.FirstOrDefault(s => s.Id == id);
+            if (statistic == null)
+            {
+                return NotFound();
+            }
+
+            return View(statistic);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var statistic = context.Statistics.Find(id);
+            if (statistic == null)
+            {
+                return NotFound();
+            }
+
+            return View(statistic);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Statistic statistic)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Update(statistic);
+                context.SaveChanges();
+                return RedirectToAction(nameof(Details), new { id = statistic.Id });
+            }
+            return View(statistic);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var statistic = context.Statistics.Find(id);
+            if (statistic == null)
+            {
+                return NotFound();
+            }
+
+            return View(statistic);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var statistic = context.Statistics.Find(id);
+            context.Statistics.Remove(statistic);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new Statistic());
+        }
+
+
+        [HttpPost]
+        public IActionResult Add(Statistic statistic)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(statistic);
+                context.SaveChanges();
+                return RedirectToAction(nameof(Details), new { id = statistic.Id });
+            }
+            return View(statistic);
+        }
     }
 }
