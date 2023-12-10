142b208e1761f93898585a9e931fbe1f3095af94
rddeters <134734702+rddeters@users.noreply.github.com>
2023-12-10 13:03:01 -0600

    Add exception handling

--- Controllers/StatisticController.cs
+++ Controllers/StatisticController.cs
@@ -7,76 +7,142 @@ namespace VolleyballFinal.Controllers
     public class StatisticController : Controller
     {
         private TeamContext context { get; set; }
-        public StatisticController(TeamContext ctx) => context = ctx;
+        private readonly ILogger<StatisticController> _logger;
+
+        public StatisticController(TeamContext ctx, ILogger<StatisticController> logger)
+        {
+            context = ctx;
+            _logger = logger;
+        }
 
         public IActionResult Index()
         {
-            var players = context.Player.ToList();
-            return View(players);
+            try
+            {
+                var players = context.Player.ToList();
+                return View(players);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing request in PlayerController");
+                return RedirectToAction("Error", "Home");
+            }
         }
-
         public IActionResult Details(int id)
         {
-            var statistic = context.Statistics.FirstOrDefault(s => s.Id == id);
-            if (statistic == null)
+            try
             {
-                return NotFound();
-            }
+                var statistic = context.Statistics.FirstOrDefault(s => s.Id == id);
+                if (statistic == null)
+                {
+                    return RedirectToAction(nameof(Add), new { id = id });
+                }
 
-            return View(statistic);
+                return View(statistic);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing request in StatisticController");
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         [HttpGet]
         public IActionResult Edit(int id)
         {
-            var statistic = context.Statistics.Find(id);
-            if (statistic == null)
+            try
             {
-                return NotFound();
-            }
+                var statistic = context.Statistics.Find(id);
+                if (statistic == null)
+                {
+                    return NotFound();
+                }
 
-            return View(statistic);
+                return View(statistic);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing request in PlayerController");
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         [HttpPost]
         public IActionResult Edit(Statistic statistic)
         {
-            if (ModelState.IsValid)
+            try
             {
-                context.Update(statistic);
-                context.SaveChanges();
-                return RedirectToAction(nameof(Details), new { id = statistic.Id });
+                if (ModelState.IsValid)
+                {
+                    context.Update(statistic);
+                    context.SaveChanges();
+                    return RedirectToAction(nameof(Details), new { id = statistic.Id });
+                }
+                return View(statistic);
             }
-            return View(statistic);
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing request in PlayerController");
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         [HttpGet]
         public IActionResult Delete(int id)
         {
-            var statistic = context.Statistics.Find(id);
-            if (statistic == null)
+            try
             {
-                return NotFound();
-            }
+                var statistic = context.Statistics.Find(id);
+                if (statistic == null)
+                {
+                    return NotFound();
+                }
 
-            return View(statistic);
+                return View(statistic);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing request in PlayerController");
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         [HttpPost, ActionName("Delete")]
         public IActionResult DeleteConfirmed(int id)
         {
-            var statistic = context.Statistics.Find(id);
-            context.Statistics.Remove(statistic);
-            context.SaveChanges();
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                var statistic = context.Statistics.Find(id);
+                context.Statistics.Remove(statistic);
+                context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing request in PlayerController");
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         [HttpGet]
-        public IActionResult Add()
+        public IActionResult Add(int? id)
         {
-            return View(new Statistic());
-        }
+            try
+            {
+                var statistic = new Statistic();
+                if (id.HasValue)
+                {
+                    statistic.Id = id.Value;
+                }
 
+                return View(statistic);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing request in StatisticController");
+                return RedirectToAction("Error", "Home");
+            }
+        }
 
         [HttpPost]
         public IActionResult Add(Statistic statistic)
@@ -89,5 +155,6 @@ namespace VolleyballFinal.Controllers
             }
             return View(statistic);
         }
+
     }
 }
