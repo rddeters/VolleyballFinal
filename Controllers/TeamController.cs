e3b68792258b566e5743daeb69d39454e98a280d
rddeters <134734702+rddeters@users.noreply.github.com>
2023-12-10 13:02:33 -0600

    Add exception handling

--- Controllers/TeamController.cs
+++ Controllers/TeamController.cs
@@ -6,89 +6,154 @@ namespace VolleyballFinal.Controllers
 {
     public class TeamController : Controller
     {
-        private TeamContext context { get; set; }
-        public TeamController(TeamContext ctx) => context = ctx;
+        private readonly TeamContext context;
+        private readonly ILogger<TeamController> _logger;
+
+        public TeamController(TeamContext ctx, ILogger<TeamController> logger)
+        {
+            context = ctx;
+            _logger = logger;
+        }
 
         public IActionResult Index()
         {
-            var teams = context.Teams.ToList(); 
-            return View(teams); 
+            try
+            {
+                var teams = context.Teams.ToList();
+                return View(teams);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error in Index action of TeamController");
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         public IActionResult FilterByLeague(string leaguetype)
         {
-            IEnumerable<Team> teams;
-
-            if (leaguetype.ToLower() == "all")
+            try
             {
-                teams = context.Teams.OrderBy(t => t.TeamName).ToList();
-            }
-            else
-            {
-                leaguetype = leaguetype.ToLower();
-                teams = context.Teams.Where(t => t.LeagueType.ToLower() == leaguetype)
-                                     .OrderBy(t => t.TeamName).ToList();
-            }
+                IEnumerable<Team> teams;
 
-            return View("Index", teams);
+                if (leaguetype.ToLower() == "all")
+                {
+                    teams = context.Teams.OrderBy(t => t.TeamName).ToList();
+                }
+                else
+                {
+                    leaguetype = leaguetype.ToLower();
+                    teams = context.Teams.Where(t => t.LeagueType.ToLower() == leaguetype)
+                                         .OrderBy(t => t.TeamName).ToList();
+                }
+
+                return View("Index", teams);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing request in TeamController");
+                return RedirectToAction("Error", "Home");
+            } 
         }
 
         public IActionResult Details(int id)
         {
-            var team = context.Teams.FirstOrDefault(t => t.Id == id);
-            if (team == null)
+            try
             {
-                return NotFound();
+                var team = context.Teams.FirstOrDefault(t => t.Id == id);
+                if (team == null)
+                {
+                    return NotFound();
+                }
+
+                var players = context.Player.Where(p => p.TeamName == team.TeamName).ToList();
+
+                var viewModel = new TeamDetailsViewModel
+                {
+                    Team = team,
+                    Players = players
+                };
+
+                return View(viewModel);
             }
-
-            var players = context.Player.Where(p => p.TeamName == team.TeamName).ToList();
-
-            var viewModel = new TeamDetailsViewModel
+            catch (Exception ex)
             {
-                Team = team,
-                Players = players
-            };
-
-            return View(viewModel);
+                _logger.LogError(ex, "An error occurred while processing request in TeamController");
+                return RedirectToAction("Error", "Home");
+            }
         }
+
         [Authorize]
         [HttpGet]
         public IActionResult Edit(int id)
         {
-            ViewBag.Action = "Edit";
-            var team = context.Teams.Find(id);
-            return View(team);
+            try
+            {
+                ViewBag.Action = "Edit";
+                var team = context.Teams.Find(id);
+                return View(team);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing request in TeamController");
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         [HttpPost]
         public IActionResult Edit(Team team)
         {
-            if(ModelState.IsValid)
+            try
             {
-                if(team.Id == 0)context.Teams.Add(team);
-                else context.Teams.Update(team);
-                context.SaveChanges();
-                return RedirectToAction("Index", "Home");
-            } else
+                if (ModelState.IsValid)
+                {
+                    if (team.Id == 0) context.Teams.Add(team);
+                    else context.Teams.Update(team);
+                    context.SaveChanges();
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.Action = (team.Id == 0) ? "Add" : "Edit";
+                    return View(team);
+                }
+            }
+            catch (Exception ex)
             {
-                ViewBag.Action = (team.Id == 0) ? "Add" : "Edit";
-                return View(team);
+                _logger.LogError(ex, "An error occurred while processing request in TeamController");
+                return RedirectToAction("Error", "Home");
             }
         }
+
         [Authorize]
         [HttpGet]
         public IActionResult Delete(int id)
         {
-            var team = context.Teams.Find(id);
-            return View(team);
+            try
+            {
+                var team = context.Teams.Find(id);
+                return View(team);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing request in TeamController");
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         [HttpPost]
         public IActionResult Delete(Team team)
         {
-            context.Teams.Remove(team);
-            context.SaveChanges();
-            return RedirectToAction("Index", "Home");
+            try
+            {
+                context.Teams.Remove(team);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing request in TeamController");
+                return RedirectToAction("Error", "Home");
+            }
         }
     }
 }
