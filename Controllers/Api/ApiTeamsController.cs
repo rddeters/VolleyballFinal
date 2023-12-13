46f2618be11d15cd38ca22f9cdd7b158d380ac4c
rddeters <134734702+rddeters@users.noreply.github.com>
2023-12-12 21:56:06 -0600

    Add files via upload

---
+++ Controllers/Api/ApiTeamsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VolleyballFinal.Models;
+
+namespace VolleyballFinal.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiTeamsController : ControllerBase
+    {
+        private readonly TeamContext _context;
+
+        public ApiTeamsController(TeamContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ApiTeams
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Team>>> GetTeams()
+        {
+          if (_context.Teams == null)
+          {
+              return NotFound();
+          }
+            return await _context.Teams.ToListAsync();
+        }
+
+        // GET: api/ApiTeams/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Team>> GetTeam(int id)
+        {
+          if (_context.Teams == null)
+          {
+              return NotFound();
+          }
+            var team = await _context.Teams.FindAsync(id);
+
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return team;
+        }
+
+        // PUT: api/ApiTeams/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTeam(int id, Team team)
+        {
+            if (id != team.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(team).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TeamExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ApiTeams
+        [HttpPost]
+        public async Task<ActionResult<Team>> PostTeam(Team team)
+        {
+          if (_context.Teams == null)
+          {
+              return Problem("Entity set 'TeamContext.Teams'  is null.");
+          }
+            _context.Teams.Add(team);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTeam", new { id = team.Id }, team);
+        }
+
+        // DELETE: api/ApiTeams/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTeam(int id)
+        {
+            if (_context.Teams == null)
+            {
+                return NotFound();
+            }
+            var team = await _context.Teams.FindAsync(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            _context.Teams.Remove(team);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TeamExists(int id)
+        {
+            return (_context.Teams?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
